f43caf17a33daff0efe449ff568cff3b4a53942e
Yauheni Hanchar <eugene.gonchar.w@gmail.com>
2023-11-26 22:56:24 +0100

    Completed 8_10

--- src/main/java/org/example/chapter8/recursion_and_dynamic_programming/Task8_10.java
+++ src/main/java/org/example/chapter8/recursion_and_dynamic_programming/Task8_10.java
@@ -3,6 +3,43 @@ package org.example.chapter8.recursion_and_dynamic_programming;
 public class Task8_10 {
 
   public static void main(String[] args) {
-    
+    String[][] screen = {
+      {" ", " ", " ", " ", " ", " ", " "},
+      {" ", " ", ".", " ", ".", " ", " "},
+      {" ", " ", ".", " ", " ", " ", " "},
+      {" ", ".", ".", " ", ".", ".", " "},
+      {".", " ", ".", ".", ".", " ", "."},
+      {" ", ".", " ", " ", " ", ".", " "}
+    };
+    printScreen(screen);
+    paintFill(screen, 3, 3, "o", screen[3][3]);
+    printScreen(screen);
+  }
+
+  private static void paintFill(
+      String[][] screen, int r, int c, String targetColor, String colorToFill) {
+    if (r < 0
+        || c < 0
+        || r >= screen.length
+        || c >= screen[0].length
+        || !screen[r][c].equals(colorToFill)) {
+      return;
+    }
+
+    screen[r][c] = targetColor;
+    paintFill(screen, r + 1, c, targetColor, colorToFill);
+    paintFill(screen, r - 1, c, targetColor, colorToFill);
+    paintFill(screen, r, c + 1, targetColor, colorToFill);
+    paintFill(screen, r, c - 1, targetColor, colorToFill);
+  }
+
+  private static void printScreen(String[][] screen) {
+    for (var row : screen) {
+      for (var pixel : row) {
+        System.out.print(pixel + " ");
+      }
+      System.out.println();
+    }
+    System.out.println();
   }
 }
