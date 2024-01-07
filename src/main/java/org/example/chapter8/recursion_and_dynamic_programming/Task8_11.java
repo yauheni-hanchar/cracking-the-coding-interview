fd0b21354e5c89b6e6ceabbe6f6bf190d174c4ad
Yauheni Hanchar <eugene.gonchar.w@gmail.com>
2024-01-07 13:07:43 +0100

    Completed 8_11, 8_12

---
+++ src/main/java/org/example/chapter8/recursion_and_dynamic_programming/Task8_11.java
@@ -0,0 +1,35 @@
+package org.example.chapter8.recursion_and_dynamic_programming;
+
+import java.util.HashMap;
+
+public class Task8_11 {
+
+  public static void main(String[] args) {
+    var result = calculateWays2(100000);
+    System.out.println("result = " + result);
+  }
+
+  private static long calculateWays2(int n) {
+    int[] denoms = {25, 10, 5, 1};
+    int[][] map = new int[n + 1][denoms.length];
+    return calculateWays2(n, denoms, 0, map);
+  }
+
+  private static long calculateWays2(int n, int[] denoms, int index, int[][] map) {
+    var cached = map[n][index];
+    if(cached > 0) {
+      return cached;
+    }
+    if(index >= denoms.length - 1) {
+      return 1;
+    }
+    var denomsAmount = denoms[index];
+    var ways = 0;
+    for(int i = 0; i * denomsAmount <= n; i++) {
+      var amountRemaining = n - i * denomsAmount;
+      ways += calculateWays2(amountRemaining, denoms, index + 1, map);
+    }
+    map[n][index] = ways;
+    return ways;
+  }
+}
---
+++ src/main/java/org/example/chapter8/recursion_and_dynamic_programming/Task8_12.java
@@ -0,0 +1,58 @@
+package org.example.chapter8.recursion_and_dynamic_programming;
+
+public class Task8_12 {
+
+  public static void main(String[] args) {
+    var n = 4;
+    printArrangements(n);
+  }
+
+  public static void printArrangements(int n) {
+    var columns = new int[n];
+    var mainDiagonal = new int[2 * n - 1];
+    var antiDiagonal = new int[2 * n - 1];
+    setQueen(n, 0, columns, mainDiagonal, antiDiagonal);
+  }
+
+  private static void setQueen(
+      int n, int column, int[] rows, int[] mainDiagonal, int[] antiDiagonal) {
+    if (column == n) {
+      printChessboard(n, rows);
+      return;
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (rows[i] == 0 && mainDiagonal[column - i + n - 1] == 0 && antiDiagonal[column + i] == 0) {
+        var queenNumber = column + 1;
+        rows[i] = queenNumber;
+        mainDiagonal[column - i + n - 1] = queenNumber;
+        antiDiagonal[column + i] = queenNumber;
+        setQueen(
+                n,
+                column + 1,
+                rows,
+                mainDiagonal,
+                antiDiagonal);
+        rows[i] = 0;
+        mainDiagonal[column - i + n - 1] = 0;
+        antiDiagonal[column + i] = 0;
+      }
+    }
+  }
+
+  private static void printChessboard(int n, int[] rows) {
+    for (int currentRow = 0; currentRow < n; currentRow++) {
+      var queenCol = rows[currentRow];
+      var rowString = new StringBuilder(n * 2);
+      for (int currentCol = 0; currentCol < n; currentCol++) {
+        if (currentCol == queenCol - 1) {
+          rowString.append("■ ");
+        } else {
+          rowString.append("□ ");
+        }
+      }
+      System.out.println(rowString);
+    }
+    System.out.println();
+  }
+}
